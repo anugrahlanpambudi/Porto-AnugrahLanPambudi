7742eb0bf0639a810e5ea0d8f91914393813b673
anugrahlanpambudi <anugrahlanpambudi27@gmail.com>
2025-11-10 15:19:34 +0700

    first commit

---
+++ resources/views/admin/home/create.blade.php
@@ -0,0 +1,33 @@
+@extends('admin.app')
+@section('title', 'Home Create')
+@section('content')
+    @if ($errors->any())
+        @foreach ($errors->all() as $error )
+            <h1>{{ $error }}</h1>
+        @endforeach
+
+    @endif
+
+    <form action="{{ route('homeadmin.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-2">
+            <label for="" class="form-label">Inage</label>
+            <input type="file" class="form-control" name="image" accept=".jpg,.png,.jpeg">
+        </div>
+        <div class="mb-2">
+            <label for="" class="form-label">Subtitle</label>
+            <input type="text" class="form-control" name="subtitle" id="">
+        </div>
+        <div class="mb-2">
+            <label for="" class="form-label">Title</label>
+            <input type="text" class="form-control" name="title" id="">
+        </div>
+        <div class="mb-2">
+            <label for="" class="form-label">Description</label>
+            <textarea class="form-control" name="description" cols="30" rows="5" id=""></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-info">Add</button>
+        <a href="{{ url('homeadmin') }}" class="btn btn-secondary">Back</a>
+    </form>
+@endsection
---
+++ resources/views/admin/inc/js.blade.php
@@ -0,0 +1,13 @@
+<!-- build:js assets/vendor/js/core.js -->
+    <script src="{{asset ('adm/assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{asset ('adm/assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{asset ('adm/assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{asset ('adm/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+    <script src="{{asset ('adm/assets/vendor/js/menu.js') }}"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="{{asset ('adm/assets/js/main.js') }}"></script>
